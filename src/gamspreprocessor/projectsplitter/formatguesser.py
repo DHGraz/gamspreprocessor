50748597f18a7e951a60295713abb833c258c61a
Gunter Vasold <gunter.vasold@uni-graz.at>
2024-08-30 14:17:37 +0200

    Update formatguesser.py
    * CI showed, that XML files sometimes were detected als 'text/xml'-
       Fixed code, so now guess_format() always returns 'application.xml'.

--- src/gamspreprocessor/projectsplitter/formatguesser.py
+++ src/gamspreprocessor/projectsplitter/formatguesser.py
@@ -43,6 +43,11 @@ def guess_format(filename: str, explicit_type: str = "auto") -> tuple[str, str]:
     # TODO: Add more formats
     # TODO: Using an external lib like fido or fits might be a good idea.
     content_type = mimetypes.guess_type(filename)[0]
+    # guess_type returns text/xml or application/xml on different platforms?!?
+    # At least the github runner returns text/xml while my local system returns application/xml ...
+    print(content_type)
+    if content_type == "text/xml": 
+        content_type = "application/xml"
     if explicit_type == "auto":
         if content_type == "application/xml":
             file_format = _guess_xml_format(filename)
