9641a174b5a41888ebe43bf6906e6beaf2fdc835
Gunter Vasold <gunter.vasold@uni-graz.at>
2025-06-27 13:05:46 +0200

    Update: warnings, CHANGELOG
    - Again reflect changes in gamslib: objectcsv no longer emmits warnings for falling back to other languages, but debugs to logger again. Reason: To many warings in console output.
    - Extend changelog

--- src/gamspreprocessor/cli/objectcsv.py
+++ src/gamspreprocessor/cli/objectcsv.py
@@ -11,10 +11,6 @@ import gamslib.projectconfiguration
 
 logger = logging.getLogger()
 
-def warning_to_debug(msg, *args, **kwargs):
-    """Convert warnings to debug messages."""
-    logger.debug(msg, *args, **kwargs)
-
 @click.group(name="csv")
 def cli():
     """Helpers for managing GAMS object CSV files."""
@@ -86,20 +82,19 @@ def createcsv(
             )
 
     cfg = gamslib.projectconfiguration.get_configuration(config_path)
+    
     if update:
-        with warnings.catch_warnings(action="ignore"):
-            csv_objects = gamslib.objectcsv.create_csv_files(
-                Path(projectroot), cfg, update=True
+        csv_objects = gamslib.objectcsv.create_csv_files(
+            Path(projectroot), cfg, update=True
             )
         click.echo(
             f"Updated csv files for {len(csv_objects)} objects "
             f"({sum(obj.count_datastreams() for obj in csv_objects)} content files)."
         )
     else:  # create new csv files
-        with warnings.catch_warnings(action="ignore"):
-            csv_objects = gamslib.objectcsv.create_csv_files(
-                    Path(projectroot), cfg, force_overwrite
-            )
+        csv_objects = gamslib.objectcsv.create_csv_files(
+                Path(projectroot), cfg, force_overwrite
+        )
         click.echo(
             f"Created csv files for {len(csv_objects)} objects "
             f"({sum(obj.count_datastreams() for obj in csv_objects)} content files)."
